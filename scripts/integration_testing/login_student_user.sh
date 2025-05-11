4a9dd35d97b53142a82e9146212f0a85a9f5b123
jasonjente <jasonjente@gmail.com>
2025-05-11 20:52:35 +0300

    init commit of the backend services. Creation of Login and Register endpoints.
    Check progress.md & README.md for more.

--- scripts/integration_testing/login_student_user.sh
+++ scripts/integration_testing/login_student_user.sh
@@ -1,7 +1,9 @@
-curl -v -X POST http://localhost:8080/api/auth/login \
-  -H "Content-Type: application/json" \
-  -d '{
-        "username": "ichatzop",
-        "password": "mysecurepass"
-      }
+curl --location 'localhost:8080/api/auth/login' \
+--header 'Content-Type: application/json' \
+--header 'Accept: application/json' \
+--header 'Cookie: JSESSIONID=5E2267C3E9E253417D48225CBBCBD7EE' \
+--data '{
+  "username": "ichatzop",
+  "password": "mysecurepass"
+}
 '
--- scripts/integration_testing/register_user.sh
+++ scripts/integration_testing/register_user.sh
@@ -1,3 +1,10 @@
-curl -i -c cookies.txt -X POST http://localhost:8080/register \
-  -H "Content-Type: application/x-www-form-urlencoded" \
-  -d "firstName=Alice&lastName=Smith&username=asmith&password=password123"
+curl --location 'localhost:8080/api/auth/register' \
+--header 'Content-Type: application/json' \
+--header 'Accept: application/json' \
+--header 'Cookie: JSESSIONID=5E2267C3E9E253417D48225CBBCBD7EE' \
+--data '{
+    "firstName": "Iason",
+    "lastName": "Chatzopoulos",
+    "username": "ichatzop2",
+    "password": "mysecurepass"
+}'
